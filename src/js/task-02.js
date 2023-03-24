d4fe91505e9ee30265fae56b369391adc558281e
SochaStanislaw <socha.stanislaw95@gmail.com>
2023-03-24 16:45:11 +0100

    backup2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,22 @@
+// Napisz skrypt, który dla każdego elementu tablicy ingredients:
+// 1. Utworzy oddzielny element <li>. Obowiązkowo użyj metody document.createElement().
+// 2. Doda nazwę elementu (string z tablicy) jako jego zawartość tekstową.
+// 3. Doda do elementu klasę item.
+// 4. Następnie jedna operacja umieści wszystkie <li> na liście ul#ingredients.
+
+const list = document.querySelector("ul");
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const item = ingredients
+  .map((ingredient) => `<li class="item">${ingredient}</li>`)
+  .join("");
+
+list.innerHTML = item;
